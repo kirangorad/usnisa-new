5d1acb25c02706a807eac74d4123d901a96f0c4a
31585 <31585>
2017-02-18 03:39:07 +0530

    Port change

--- usnisa/public/controllers/config.js
+++ usnisa/public/controllers/config.js
@@ -1,6 +1,6 @@
-//var baseURL = 'http://usnisa.in';
+var baseURL = 'http://usnisa.in:8000';
 //var baseURL = 'http://52.66.21.137';
-var baseURL = 'http://localhost:8000';
+//var baseURL = 'http://localhost:8000';
 
 // Module specific configuration
 angular.module('app.config',[])
