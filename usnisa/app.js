fb68e4cee6a7d5a257a14239fd959ec64ec89627
31585 <31585>
2017-03-03 01:24:50 +0530

    Enable Server Connection Compression

--- usnisa/app.js
+++ usnisa/app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var compression = require('compression')
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -27,7 +28,7 @@ require('./config/passport')(passport); // pass passport for configuration
 
 var app = express();
 
-
+app.use(compression());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'public'));
